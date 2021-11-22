93af37900eed92704ffc18dc8e41e1e0210cd072
kmiqbal19 <km.iqbal01@gmail.com>
2021-11-22 21:38:05 +0100

    add more styling in service section

--- src/components/Servicesection.component/service.component.jsx
+++ src/components/Servicesection.component/service.component.jsx
@@ -4,28 +4,25 @@ import "./service.component.css";
 function ServiceSection() {
   return (
     <section className="service-section">
+      <h1> OUR SERVICES </h1>
       <div className="service-item trophy">
         <i class="fas fa-trophy"></i>
         <p className="service-text">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,
+          Lorem Ipsum is simply dummy text of the printing and typesetting.
         </p>
       </div>
       <div className="service-item camera">
         <i class="fas fa-camera-retro"></i>
         <p className="service-text">
           <NavLink to="/gallery">#MCFCBD GALLERY</NavLink> simply dummy text of
-          the printing and typesetting industry. Lorem Ipsum has been the
-          industry's standard dummy text ever since the 1500s,
+          the printing ands.
         </p>
       </div>
       <div className="service-item flag">
         <i class="fas fa-flag"></i>
         <p className="service-text">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,
+          indus.
         </p>
       </div>
     </section>
