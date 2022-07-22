0575027f81aca26aaf66e441f3b1d266d3d57402
kmiqbal19 <km.iqbal01@gmail.com>
2022-07-23 00:13:15 +0200

    finalized service section

--- src/components/Servicesection.component/service.component.jsx
+++ src/components/Servicesection.component/service.component.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./service.component.css";
+import { FaTrophy, FaPhotoVideo, FaFlag } from "react-icons/fa";
+
 function ServiceSection() {
   return (
     <section className="service-section">
       <h1> OUR SERVICES </h1>
       <div className="service-item trophy">
-        <i class="fas fa-trophy"></i>
+        <FaTrophy className="fa-trophy" />
         <p className="service-text">
           <NavLink to="/join">JOIN</NavLink> To get official membership join our
           branch
         </p>
       </div>
       <div className="service-item camera">
-        <i class="fas fa-camera-retro"></i>
+        <FaPhotoVideo className="fa-gallery" />
         <p className="service-text">
           <NavLink to="/gallery">#MCFCBD GALLERY </NavLink> Tag your photos
           using #mcfcbd when you share your photos
         </p>
       </div>
       <div className="service-item flag">
-        <i class="fas fa-flag"></i>
+        <FaFlag className="fa-flag" />
         <p className="service-text">
           <a href="https://www.mancity.com/fixtures">FIXTURES </a>
           Premier League Champions League FA CUP
